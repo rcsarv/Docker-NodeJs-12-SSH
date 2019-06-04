c6bafde77d73ea8e59dcdedc99ed63db1eb2842b
RC Sarv <rc@sarv.com>
2019-06-04 15:59:58 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
-echo "Hi";
+function logEvery2Seconds(i) {
+    setTimeout(() => {
+        console.log('Infinite Loop Test n:', i);
+        logEvery2Seconds(++i);
+    }, 2000)
+}
+
+logEvery2Seconds(0);
+
+let i = 0;
+setInterval(() => {
+    console.log('Infinite Loop Test interval n:', i++);
+}, 2000)
